15b4a7beb095fa209f553a8625b76db127e96bc7
Anthony3064 <anthonyestiven3064@gmail.com>
2024-02-28 14:41:25 -0600

    First commit

---
+++ src/main/java/com/cci/demohello/controller/MainController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cci.demohello.controller;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Anthony Flores Boza
+ */
+@RestController
+@RequestMapping("/hello-world")
+public class MainController {
+
+    @GetMapping(path = {"", "/"})
+    public ResponseEntity<?> hello() {
+        Map<String, Object> response = new HashMap<String, Object>() {
+            {
+                put("message", "Hello World");
+                put("date", LocalDate.now());
+            }
+        };
+        return ResponseEntity.ok(response);
+    }
+}
