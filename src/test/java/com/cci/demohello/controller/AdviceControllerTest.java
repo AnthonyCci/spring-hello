eb8b1a27bb4222ffe076e82849df42fd8a11b4c1
Anthony3064 <anthonyestiven3064@gmail.com>
2024-03-08 22:55:04 -0600

    Updated controlleradvice test

--- src/test/java/com/cci/demohello/controller/AdviceControllerTest.java
+++ src/test/java/com/cci/demohello/controller/AdviceControllerTest.java
@@ -1,46 +1,37 @@
 package com.cci.demohello.controller;
 
-import com.cci.demohello.exception.BadRequestException;
 import com.cci.demohello.exception.config.GlobalExceptionHandler;
 import com.cci.demohello.model.ResponseExceptionDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdviceControllerTest {
-    @Mock
-    private HttpServletRequest request;
-
-    @Mock
-    private HttpServletResponse response;
-
-    @InjectMocks
-    private GlobalExceptionHandler adviceController;
 
     @Test
     public void testHandleException() {
-        Exception ex = new Exception("Test exception");
-        ResponseEntity<ResponseExceptionDTO> response = adviceController.handlerGenericException(ex);
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatusCode().value());
-        assertEquals("Test exception", response.getBody().getMessage());
-    }
+        // Mock de HttpServletRequest y HttpServletResponse
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
 
-    @Test
-    public void testHandleBadRequestException() {
-        BadRequestException ex = new BadRequestException("Test exception");
-        ResponseEntity<ResponseExceptionDTO> response = adviceController.handleBadRequestException(ex);
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatusCode().value());
-        assertEquals("Test exception", response.getBody().getMessage());
+        // Instancia del controlador de excepciones
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        // Lanzar una excepción dentro del método handlerGenericException
+        Exception exception = new Exception("Test exception");
+        ResponseEntity<ResponseExceptionDTO> responseEntity = globalExceptionHandler.handlerGenericException(exception);
+
+        // Verificar que se genera una respuesta con el código de estado correcto y el mensaje de la excepción
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getBody().getStatus());
+        assertEquals("Test exception", responseEntity.getBody().getMessage());
     }
 }
