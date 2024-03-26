0b0e0069c02c447a1a967aee7c1db6b111ded3c0
Anthony3064 <anthonyestiven3064@gmail.com>
2024-03-26 15:19:04 -0500

    Cambie el mensaje del main

--- src/test/java/com/cci/demohello/controller/MainControllerTest.java
+++ src/test/java/com/cci/demohello/controller/MainControllerTest.java
@@ -38,6 +38,7 @@ public class MainControllerTest {
         );
         response.andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message", is("Server is running Anthony")));
+                .andExpect(jsonPath("$.message", is("Server is running Bernal")));
     }
+             
 }
